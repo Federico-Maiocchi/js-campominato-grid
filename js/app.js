fdb07da7731b6462b74558628e21ff8222296fbe
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-13 16:26:57 +0200

    ADD: bg light blue in all cell

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@ const btnPlayDomElement = document.getElementById('btn-play');
 btnPlayDomElement.addEventListener('click', function () {
     // console.log('click')
 
+    // cancello le celle precedenti
     gridDomeElement.innerHTML = ''
+
     // usiamo il ciclo for per ricavare i numeri da 1 a 100
     // che metteremo nelle celle
     for (let i = 0; i < 100; i++ ) {
@@ -22,11 +24,26 @@ btnPlayDomElement.addEventListener('click', function () {
 
     // inizializziamo una costante dove creeremo dei div nel DOM(html)
     const htmlcell = `<div class="cell">${n}</div>`
-    console.log(htmlcell);
+    // console.log(htmlcell);
 
     // importiamo i div creati nel dom
     gridDomeElement.innerHTML += htmlcell;
+    
+    }
 
+    // recupero tutte celle create
+    const cellDomElements = document.querySelectorAll('.cell');
+    console.log(cellDomElements);
+
+    // creo un arrei con gli elementi delle cell
+    for (let i = 0; i < cellDomElements.length; i++) {
+        const currentCellElement = cellDomElements[i];
+        console.log(currentCellElement);
+
+        // ogni elemento avrà l'evento click
+        currentCellElement.addEventListener('click', function () {
+            currentCellElement.classList.add('bg-light-blue');
+        }) 
 
     }
 
